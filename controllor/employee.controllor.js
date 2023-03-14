2cfefb68fff901b5584968e7dc28743d355b15c1
Mahesh-WAL <mahesh.m@westagilelabs.com>
2023-03-14 12:17:33 +0530

    "added forgot and reset password routes"

--- controllor/employee.controllor.js
+++ controllor/employee.controllor.js
@@ -1,11 +1,16 @@
 //import express async handler
 const expressAsyncHandler=require('express-async-handler');
+//import bcrypt js
 const bcryptjs=require('bcryptjs');
+//import nodemailer
+const nodemailer=require('nodemailer');
+
 require('dotenv').config();
 //import json web token
 const jwt=require("jsonwebtoken");
 //import employee model
 const {Employees}=require('../database/models/employee.model');
+
 //employee registration
 exports.employeeRegistration=expressAsyncHandler(async(req,res)=>{
     let {employee_id,employee_name,age,gender,email,password,role}=req.body;
@@ -21,6 +26,7 @@ exports.employeeRegistration=expressAsyncHandler(async(req,res)=>{
     });
     res.send({message:"employee registered"})
 });
+
 //employee login
 exports.employeeLogin=expressAsyncHandler(async(req,res)=>{
     let {email,password}=req.body;
@@ -42,3 +48,62 @@ exports.employeeLogin=expressAsyncHandler(async(req,res)=>{
         }
     }
 })
+
+//create connection to smtp
+const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_SERVICE_PROVIDER,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD, // app password
+    },
+});
+//to store the otp
+let otps = {};
+
+//forgot password
+exports.forgotPassword=expressAsyncHandler(async(req,res)=>{
+    //generate otp
+    let otp = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
+    console.log(otp); //176717
+    otps[req.body.email] = otp;
+    console.log(req.body.email);
+    // defining mail options
+    let mailOptions = {
+        from: "mahesh",
+        to: req.body.email,
+        subject: "OTP to reset password for project pulse",
+        text: `Hi, OTP to reset password is : ` + otp,
+    };
+
+    // send email
+    transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.log("err");
+    } 
+    else {
+      console.log("email sent", info.messageId);
+    }
+    })
+    res.send({message:"otp sent by email"})
+});
+
+//reset password
+exports.resetPassword = expressAsyncHandler(async (req, res) => {
+    // checking if the otp is valid
+    //otp matches
+    if (req.body.otp == otps[req.params.email]) {
+      console.log("password verified");
+      let hashedPassword = await bcryptjs.hash(req.body.password, 6);
+      console.log("email", req.params.email);
+      let updatedCount = await Employees.update({ password: hashedPassword },
+        {
+          where: {email: req.params.email},
+        }
+      );
+      res.send({ message: "Password reset sucessfully" });
+    }
+    // else
+    else {
+      res.send({ message: "Invalid OTP" });
+    }
+});
--- routes/employee.route.js
+++ routes/employee.route.js
@@ -5,11 +5,18 @@ const employeeApp=express.Router();
 //body parser
 employeeApp.use(express.json());
 
-const {employeeRegistration,employeeLogin} =require('../controllor/employee.controllor');
+const {employeeRegistration,employeeLogin,forgotPassword,resetPassword} =require('../controllor/employee.controllor');
 //routes
 //employee registration
 employeeApp.post('/employee-registration',employeeRegistration );
 
 //employee login
 employeeApp.post('/employee-login',employeeLogin)
+
+//forgot password
+employeeApp.post('/forgot-password',forgotPassword);
+
+//reset passsword
+employeeApp.put('/reset-password/:email',resetPassword);
+
 module.exports = employeeApp;
